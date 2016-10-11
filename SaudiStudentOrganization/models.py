221caf718c5aa53c2ef5b9b05c72764ae65eac44
dmcelroy-UD <dmcelroy@dbq.edu>
2016-10-10 20:30:58 -0500

    Change to model from class on 10/10/2016

--- SaudiStudentOrganization/models.py
+++ SaudiStudentOrganization/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    Frist_Name = models.CharField(max_length=200)
+    Last_Name = models.CharField(max_length=200)
+    Email = models.CharField(max_length=200)
+    Phone_Number = models.CharField(max_length=200)
+
+class Responds(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    Response_Date_Time = models.DateTimeField('date responded')
